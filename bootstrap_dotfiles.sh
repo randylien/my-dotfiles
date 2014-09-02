c2c1f20da9f4b8b97e57db69eb6ee8dd68c72ead
Randy Lien <randylien@kkbox.com>
2014-09-02 15:21:11 +0800

    add .gitconfig & .gitignore_global into bootstrap list

--- bootstrap_dotfiles.sh
+++ bootstrap_dotfiles.sh
@@ -2,7 +2,7 @@
  
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim .slate.js .bash_profile"        
+files=".bashrc .vimrc .vim .slate.js .bash_profile .gitconfig .gitignore_global" 
 # list of files/folders to symlink in homedir
  
 ##########
